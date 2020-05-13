bf478ae2ef005756e69a0c04fe9cf85decfd2b5b
RehamEzzat <rehamezzatmohamed@gmail.com>
2020-05-13 15:15:24 -0700

    initial commit

---
+++ TryNewXcode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TryNewXcode
+//
+//  Created by Reham Ezzat on 5/13/20.
+//  Copyright © 2020 Reham Ezzat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
